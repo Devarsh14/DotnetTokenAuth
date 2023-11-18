93a5610fd0898d12fad15538f2f2683bf72b4377
Devarsh Shukla <devarsh14@gmail.com>
2023-11-19 09:34:47 +1000

    WIP: added basic identity context

--- DotnetTokenAuth/Models/IdentiyContext.cs
+++ DotnetTokenAuth/Models/IdentiyContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotnetTokenAuth.Models
 {
-    public class IdentiyContext : IdentityDbContext
+    public class IdentiyContext : IdentityDbContext<IdentityUser>
     {
+        public IdentiyContext(DbContextOptions options) : base(options)
+        {
+                
+        }
     }
 }
